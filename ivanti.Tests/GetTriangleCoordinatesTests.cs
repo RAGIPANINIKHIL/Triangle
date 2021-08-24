b1d603177d4dcf7518ba2c7000749c975322c0d5
Ragipani Nikhil <rnikhil@ivycomptech.com>
2021-08-25 00:57:04 +0530

    full development

---
+++ ivanti.Tests/GetTriangleCoordinatesTests.cs
@@ -0,0 +1,47 @@
+﻿using Ivanti.Controllers;
+using NUnit.Framework;
+
+namespace ivanti.Tests
+{
+
+    [TestFixture]
+    public class GetTriangleCoordinatesTests
+    {
+
+        private TriangleController triangleController;
+
+        [SetUp]
+
+        public void setup()
+        {
+            triangleController = new TriangleController();
+
+        }
+
+        [Test]
+        public void shouldNotThrowAnyErrorWhenInputIsNUll() {
+            Assert.DoesNotThrow( () => triangleController.GetTriangleCoordinates(null));
+        }
+        [Test]
+        public void shouldReturnCorrectCoordinatesdWhenTriangleColumnNumberIsEven() {
+            var trianglecoordinates = triangleController.GetTriangleCoordinates("F10");
+            Assert.AreEqual(50,trianglecoordinates.point1[0]);
+            Assert.AreEqual(0, trianglecoordinates.point1[1]);
+            Assert.AreEqual(50, trianglecoordinates.point2[0]);
+            Assert.AreEqual(10, trianglecoordinates.point2[1]);
+            Assert.AreEqual(40, trianglecoordinates.point3[0]);
+            Assert.AreEqual(10, trianglecoordinates.point3[1]);
+        }
+        [Test]
+        public void shouldReturnCorrectCoordinatesdWhenTriangleColumnNumberIsOdd()
+        {
+            var trianglecoordinates = triangleController.GetTriangleCoordinates("F9");
+            Assert.AreEqual(40, trianglecoordinates.point1[0]);
+            Assert.AreEqual(0, trianglecoordinates.point1[1]);
+            Assert.AreEqual(50, trianglecoordinates.point2[0]);
+            Assert.AreEqual(0, trianglecoordinates.point2[1]);
+            Assert.AreEqual(40, trianglecoordinates.point3[0]);
+            Assert.AreEqual(10, trianglecoordinates.point3[1]);
+        }
+    }
+}
---
+++ ivanti.Tests/GetTriangleRowAndColumnTests.cs
@@ -0,0 +1,61 @@
+﻿using Ivanti;
+using Ivanti.Controllers;
+using NUnit.Framework;
+
+
+namespace ivanti.Tests
+{
+    public class GetTriangleRowAndColumnTests
+    {
+        private TriangleController triangleController;
+
+        [SetUp]
+
+        public void setup()
+        {
+            triangleController = new TriangleController();
+
+        }
+
+        [Test]
+        public void shouldNotThrowAnyErrorWhenInputIsNUll()
+        {
+            Assert.DoesNotThrow(() => triangleController.GetTriangleRowAndColumn(null));
+        }
+        [Test]
+        public void shouldReturnCorrectRowAndColumnWhenTriangleColumnNumberIsEven()
+        {
+            var coordinates = new TriangleCoordinates() {
+                point1 = new int[2],
+                point2 = new int[2],
+                point3 = new int[2]
+            };
+            coordinates.point1[0] = 50;
+            coordinates.point1[1] = 0;
+            coordinates.point2[0] = 50;
+            coordinates.point2[1] = 10;
+            coordinates.point3[0] = 40;
+            coordinates.point3[1] = 10;
+            var triangleRowandColumn = triangleController.GetTriangleRowAndColumn(coordinates);
+            Assert.AreEqual("F10", triangleRowandColumn);
+        }
+        [Test]
+        public void shouldReturnCorrectRowAndColumnWhenTriangleColumnNumberIsOdd()
+        {
+            var coordinates = new TriangleCoordinates()
+            {
+                point1 = new int[2],
+                point2 = new int[2],
+                point3 = new int[2]
+            };
+            coordinates.point1[0] = 40;
+            coordinates.point1[1] = 0;
+            coordinates.point2[0] = 50;
+            coordinates.point2[1] = 0;
+            coordinates.point3[0] = 40;
+            coordinates.point3[1] = 10;
+            var triangleRowandColumn = triangleController.GetTriangleRowAndColumn(coordinates);
+            Assert.AreEqual("F9", triangleRowandColumn);
+        }
+    }
+}
